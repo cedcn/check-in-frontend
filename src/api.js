e5a9297ca058172b5ec641d5c41cf30a84261f97
Cedcn <cedcn@qq.com>
2017-01-12 14:14:03 +0800

    fix bug - api err

--- src/api.js
+++ src/api.js
@@ -6,14 +6,14 @@ import UUID from 'uuid-js';
     // => GET /api/gift/uncheck/<uid>/<gift>
 
 
-const get = url => axios.get(`${url}?v=${UUID.create()}`);
+const get = url => axios.get(`${url}?_=${UUID.create()}`);
 
-export const checkinCode = code => get(`/api/checkin/code/${code}/`);
-export const checkinUid = uid => get(`/api/checkin/user_id/${uid}/`);
-export const uncheckCode = code => get(`/api/uncheck/code/${code}/`);
-export const uncheckUid = uid => get(`/api/uncheck/user_id/${uid}/`);
-export const search = keyword => get(`/api/search/${keyword}/`);
-// export const giftList = uid => get(`/api/gift/list/${uid}/`);
+export const checkinCode = code => get(`/api/checkin/code/${code}`);
+export const checkinUid = uid => get(`/api/checkin/user_id/${uid}`);
+export const uncheckCode = code => get(`/api/uncheck/code/${code}`);
+export const uncheckUid = uid => get(`/api/uncheck/user_id/${uid}`);
+export const search = keyword => get(`/api/search/${keyword}`);
+// export const giftList = uid => get(`/api/gift/list/${uid}`);
 export const giftAvailable = uid => get(`/api/gift/for/${uid}/available`);
 export const checkoutGift = (uid, gift) => get(`/api/gift/checkout/${uid}/${gift}`);
 export const uncheckGift = (uid, gift) => get(`/api/gift/uncheck/${uid}/${gift}`);
